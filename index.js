7d4a627f6e7c4583cf015a0d2b8fb004535dae70
Elkwizard <bananamanthefourth@gmail.com>
2024-04-02 01:24:43 -0700

    improved code organization and added the ability to click on a location to see its zoning

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-const PX_PER_MILE = 485 / 2;
-const EQUATORIAL_RADIUS = 3963.1906;
-const POLAR_RADIUS = 3949.9028;
-const REF_LONGITUDE = -122.300880;
-const REF_LATITUDE = 47.686690;
-const REF_X = 1706;
-const REF_Y = 949;
+// zoning map
 const LEGEND_X = 2554;
 const LEGEND_Y = 1107;
 const LEGEND_INCREMENT = 68;
@@ -21,6 +15,7 @@ const LEGEND_LABELS = [
 	["as a", "Major Institution"]
 ];
 
+const output = document.getElementById("zoneView");
 const canvas = document.getElementById("markers");
 const context = canvas.getContext("2d");
 
@@ -35,6 +30,7 @@ const loaded = new Promise(resolve => {
 		imageData = context.getImageData(0, 0, canvas.width, canvas.height);
 		context.clearRect(0, 0, canvas.width, canvas.height);
 		
+		// read legend
 		for (let i = 0; i < LEGEND_LABELS.length; i++) {
 			const y = LEGEND_Y + LEGEND_INCREMENT * i;
 			const x = LEGEND_X;
@@ -51,78 +47,102 @@ const loaded = new Promise(resolve => {
 	image.src = "images/finalMap.png";
 });
 
-function getCoord(degrees, radius, baseDegrees, base) {
-	return ((degrees - baseDegrees) / 180 * Math.PI * radius) * PX_PER_MILE + base;
-}
-
+// zone detection
 function colorDistance(a, b) {
-	return Math.hypot(...a.map((v, i) => (v - b[i]) ** 2));
+	return Math.hypot(...a.map((v, i) => v - b[i]));
 }
 
-const output = document.getElementById("zoneView");
-const button = document.getElementById("findZone");
-button.addEventListener("click", () => {
-	navigator.geolocation.getCurrentPosition(position => {
-		const { longitude, latitude } = position.coords;
-		output.innerText = "Loading...";
-		loaded.then(() => {
-			const x = getCoord(longitude, EQUATORIAL_RADIUS, REF_LONGITUDE, REF_X);
-			// radius is negative because a decrease in y is an increase in latitude
-			const y = getCoord(latitude, -POLAR_RADIUS, REF_LATITUDE, REF_Y);
+function updateZone(x, y) {
+	// draw circular marker
+	context.clearRect(0, 0, canvas.width, canvas.height);
+	context.fillStyle = "blue";
+	context.beginPath();
+	context.arc(x, y, 10, 0, Math.PI * 2);
+	context.fill();
 
-			// draw circular marker
-			context.clearRect(0, 0, canvas.width, canvas.height);
-			context.fillStyle = "blue";
-			context.beginPath();
-			context.arc(x, y, 10, 0, Math.PI * 2);
-			context.fill();
+	context.fillStyle = "black";
+	context.font = "50px sans-serif";
+	context.fillText("You are here!", x + 15, y);
 
-			context.fillStyle = "black";
-			context.font = "50px sans-serif";
-			context.fillText("You are here!", x + 15, y);
+	const px = Math.floor(x);
+	const py = Math.floor(y);
+	
+	if (px < 0 || py < 0 || px >= imageData.width || py >= imageData.height) {
+		output.innerText = `Your current location is outside the map`;
+	} else {
+		const index = (py * imageData.width + px) * 4;
+		const color = [
+			imageData.data[index + 0],
+			imageData.data[index + 1],
+			imageData.data[index + 2]
+		];
 
-			const px = Math.floor(x);
-			const py = Math.floor(y);
-			
-			if (px < 0 || py < 0 || px >= imageData.width || py >= imageData.height) {
-				output.innerText = `Your current location is outside the map`;
-			} else {
-				const index = (py * imageData.width + px) * 4;
-				const color = [
-					imageData.data[index + 0],
-					imageData.data[index + 1],
-					imageData.data[index + 2]
-				];
-
-				let bestDist = Infinity;
-				let bestIndex = 0;
-				for (let i = 0; i < colors.length; i++) {
-					const target = colors[i];
-					const dist = colorDistance(color, target);
-					if (dist < bestDist) {
-						bestDist = dist;
-						bestIndex = i;
-					}
-				}
-
-				if (bestDist > 40)
-					output.innerText = `Your current location does not have a specified zone`;
-				else {
-					const [prefix, zone] = LEGEND_LABELS[bestIndex];
-					output.innerText = `Your current location is zoned ${prefix} `;
-					const zoneElement = document.createElement("span");
-					zoneElement.className = "zone";
-					zoneElement.innerText = zone;
-					zoneElement.style.background = `rgb(${colors[bestIndex].join(", ")})`;
-					output.appendChild(zoneElement);
-				}
+		let bestDist = Infinity;
+		let bestIndex = 0;
+		for (let i = 0; i < colors.length; i++) {
+			const target = colors[i];
+			const dist = colorDistance(color, target);
+			if (dist < bestDist) {
+				bestDist = dist;
+				bestIndex = i;
 			}
-		});
-	}, error => {
-		output.innerText = "An error occurred: " + error;
-	}, {
-		enableHighAccuracy: true
+		}
+
+		console.log(color, colors, bestIndex);
+
+		if (bestDist >= 62)
+			output.innerText = `Your current location does not have a specified zone`;
+		else {
+			const [prefix, zone] = LEGEND_LABELS[bestIndex];
+			output.innerText = `Your current location is zoned ${prefix} `;
+			const zoneElement = document.createElement("span");
+			zoneElement.className = "zone";
+			zoneElement.innerText = zone;
+			zoneElement.style.background = `rgb(${colors[bestIndex].join(", ")})`;
+			output.appendChild(zoneElement);
+		}
+	}
+}
+
+{ // hover zone detection
+	canvas.addEventListener("mousedown", event => {
+		console.log(1);
+		const bounds = canvas.getBoundingClientRect();
+		const x = (event.clientX - bounds.x) / bounds.width * imageData.width;
+		const y = (event.clientY - bounds.y) / bounds.height * imageData.height;
+		updateZone(x, y);
 	});
-});
+}
 
+{ // geolocation zone detection
+	const PX_PER_MILE = 485 / 2;
+	const EQUATORIAL_RADIUS = 3963.1906;
+	const POLAR_RADIUS = 3949.9028;
+	const REF_LONGITUDE = -122.300880;
+	const REF_LATITUDE = 47.686690;
+	const REF_X = 1706;
+	const REF_Y = 949;
 
+	function getCoord(degrees, radius, baseDegrees, base) {
+		return ((degrees - baseDegrees) / 180 * Math.PI * radius) * PX_PER_MILE + base;
+	}
+
+	const button = document.getElementById("findZone");
+	button.addEventListener("click", () => {
+		navigator.geolocation.getCurrentPosition(position => {
+			const { longitude, latitude } = position.coords;
+			output.innerText = "Loading...";
+			loaded.then(() => {
+				const x = getCoord(longitude, EQUATORIAL_RADIUS, REF_LONGITUDE, REF_X);
+				// radius is negative because a decrease in y is an increase in latitude
+				const y = getCoord(latitude, -POLAR_RADIUS, REF_LATITUDE, REF_Y);
+
+				updateZone(x, y);
+			});
+		}, error => {
+			output.innerText = "An error occurred: " + error;
+		}, {
+			enableHighAccuracy: true
+		});
+	});
+}
